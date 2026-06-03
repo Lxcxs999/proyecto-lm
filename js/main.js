6ad3df1aaba110e70ff87fb923ab85ef56d1b531
Lxcxs999 <lukachuflute@gmail.com>
2026-06-03 23:35:17 +0200

    v.1.7 Release Version

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
-let lastScroll = 0;
-const nav = document.querySelector('nav');
-const filterBar = document.querySelector('.filter-bar');
-const GAP = 32;
+// Detectar si estamos en la página de productos
+const isProductsPage = window.location.pathname.includes('productos.html') || window.location.pathname.includes('index.html') || window.location.pathname.includes('enfermeria.html');
 
-window.addEventListener('scroll', () => {
-  const currentScroll = window.pageYOffset;
+if (isProductsPage) {
+  let lastScroll = 0;
+  const nav = document.querySelector('nav');
+  const filterBar = document.querySelector('.filter-bar');
+  const GAP = 32;
 
-  if (currentScroll > lastScroll) {
-    nav.classList.add('nav-hidden');
+  window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
 
-    if (filterBar) {
-      filterBar.style.top = GAP + "px";
+    if (currentScroll > lastScroll) {
+      nav.classList.add('nav-hidden');
+
+      if (filterBar) {
+        filterBar.style.top = GAP + "px";
+      }
+    } else {
+      nav.classList.remove('nav-hidden');
+
+      if (filterBar) {
+        filterBar.style.top = nav.offsetHeight + GAP + 'px';
+      }
     }
-  } else {
-    nav.classList.remove('nav-hidden');
-
-    if (filterBar) {
-      filterBar.style.top = nav.offsetHeight + GAP + 'px';
-    }
-  }
-  
-  lastScroll = currentScroll;
-});
+    
+    lastScroll = currentScroll;
+  });
+}
